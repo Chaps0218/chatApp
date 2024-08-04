ee10b50a36d4185da84db3a8405e4e46fa521a54
Christopher Iza <cdiza5@espe.edu.ec>
2024-08-04 12:49:06 -0500

    Correccion Mensaje equivocado

--- front/reactProject/chat-app-front/src/components/ChatRoom.jsx
+++ front/reactProject/chat-app-front/src/components/ChatRoom.jsx
@@ -1,33 +1,34 @@
 // src/components/ChatRoom.jsx
-import { useState } from 'react';
-import { useChat } from '../hooks/useChat';
-import UsersList from './UsersList';
-import ChatArea from './ChatArea';
+import { useState } from "react";
+import { useChat } from "../hooks/useChat";
+import ChatArea from "./ChatArea";
+import UsersList from "./UsersList";
 
 function ChatRoom({ user, onLogout }) {
-    const { connectedUsers, messages, sendMessage, selectUser } = useChat(user);
-    const [selectedUser, setSelectedUser] = useState(null);
+  const { connectedUsers, messages, sendMessage, selectUser } = useChat(user);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-    const handleSelectUser = (user) => {
-        setSelectedUser(user);
-        selectUser(user.nickName);
-    };
+  const handleSelectUser = (user) => {
+    setSelectedUser(user);
+    selectUser(user.nickName);
+  };
 
-    return (
-        <div className="chat-container">
-            <UsersList
-                users={connectedUsers}
-                onSelectUser={handleSelectUser}
-                currentUser={user}
-                onLogout={onLogout}
-            />
-            <ChatArea
-                messages={messages}
-                onSendMessage={sendMessage}
-                currentUser={user}
-                selectedUser={selectedUser}
-            />
-        </div>
-    );
+  return (
+    <div className="chat-container">
+      <UsersList
+        users={connectedUsers}
+        onSelectUser={handleSelectUser}
+        currentUser={user}
+        onLogout={onLogout}
+      />
+      <ChatArea
+        messages={messages}
+        onSendMessage={sendMessage}
+        currentUser={user}
+        selectedUser={selectedUser}
+      />
+    </div>
+  );
 }
+
 export default ChatRoom;
